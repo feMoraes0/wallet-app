196298cc1e91031405343202c9ef11221ada416c
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-10-10 19:59:16 -0300

    Defined first model text to credit Card

--- lib/app/wallet/custom_card.dart
+++ lib/app/wallet/custom_card.dart
@@ -11,10 +11,108 @@ class CustomCard extends StatelessWidget {
     return Container(
       width: size.width,
       margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: (this.active) ? 0.0 : 15.0),
+      padding: const EdgeInsets.all(25.0),
       height: (this.active) ? 200.0 : 170.0,
       decoration: BoxDecoration(
-        color: Colors.pinkAccent,
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: <Color>[Colors.purpleAccent, Colors.redAccent],
+        ),
         borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Icon(
+                Icons.delete_outline,
+                size: 24.0,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "CARD NUMBER",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "**** **** **** 0000",
+                      style: TextStyle(
+                        fontSize: 31.0,
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "NAME",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "Fernando de Moraes",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      "DATE",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "10/10",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.white
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
