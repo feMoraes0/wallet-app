eee94ea4ca7c1ca9a02ed727f489a27500638512
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-10-09 22:40:13 -0300

    Added details to card screen

--- lib/app/cards/cards.dart
+++ lib/app/cards/cards.dart
@@ -10,20 +10,47 @@ class Cards extends StatelessWidget {
       backgroundColor: Colors.grey[50],
       body: SafeArea(
         child: SingleChildScrollView(
+          padding: EdgeInsets.only(bottom: 10.0),
           child: Column(
             children: <Widget>[
-              Container(
-                width: size.width,
-                padding: EdgeInsets.symmetric(vertical: 40.0),
-                child: Text(
-                  "Cards",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                  ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    InkWell(
+                      borderRadius: BorderRadius.circular(360.0),
+                      onTap: () { print("Hello World 2"); },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Icon(Icons.account_balance_wallet, size: 30.0),
+                      ),
+                    ),
+                    Text(
+                      "Cards",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 30.0,
+                      ),
+                    ),
+                    InkWell(
+                      borderRadius: BorderRadius.circular(360.0),
+                      onTap: () { print("Hello World"); },
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Icon(Icons.add, size: 30.0),
+                      ),
+                    )
+                  ],
                 ),
               ),
               CreditCard(),
+              EventCard(),
+              CreditCard(),
+              EventCard(),
+              CreditCard(),
+              EventCard(),
             ],
           ),
         ),
@@ -33,6 +60,72 @@ class Cards extends StatelessWidget {
 }
 
 class CreditCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+        width: size.width - 50.0,
+        height: 230.0,
+        margin: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 25.0),
+        padding: EdgeInsets.all(30.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[Colors.lightGreenAccent, Colors.blue],
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black45,
+              blurRadius: 10.0,
+            )
+          ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  Icons.credit_card,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "**** **** **** 0000",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  "Fernando",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+  }
+}
+
+class EventCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -60,21 +153,35 @@ class CreditCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Icon(
-            Icons.credit_card,
+            Icons.event,
             color: Colors.white,
             size: 30.0,
           ),
-          Text(
-            "**** **** **** 0000",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30.0,
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Travel",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32.0,
+                ),
+              ),
+            ],
           ),
-          Text(
-            "Fernando",
-            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                "Fernando",
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
           ),
         ],
       ),
