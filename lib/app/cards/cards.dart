1d887577773a089d73ffd2e3203944e2791dad90
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-10-09 21:52:00 -0300

    Added credit card

--- lib/app/cards/cards.dart
+++ lib/app/cards/cards.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 
 class Cards extends StatelessWidget {
@@ -5,33 +6,75 @@ class Cards extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 5.0, bottom: 15.0),
-            child: Text(
-              "My Cards",
-              style: TextStyle(
-                fontSize: 23.0
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: size.width,
+                padding: EdgeInsets.symmetric(vertical: 40.0),
+                child: Text(
+                  "Cards",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                  ),
+                ),
               ),
+              CreditCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CreditCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width - 50.0,
+      height: 230.0,
+      margin: EdgeInsets.fromLTRB(25.0, 0.0, 25.0, 25.0),
+      padding: EdgeInsets.all(30.0),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[Colors.redAccent, Colors.purpleAccent],
+          ),
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black45,
+              blurRadius: 10.0,
+            )
+          ]),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.credit_card,
+            color: Colors.white,
+            size: 30.0,
+          ),
+          Text(
+            "**** **** **** 0000",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
             ),
           ),
-          Container(
-            width: size.width - 50.0,
-            height: 200.0,
-            margin: EdgeInsets.only(bottom: 20.0),
-            decoration: BoxDecoration(
-              color: Colors.grey[100],
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey[500],
-                  offset: Offset(2.0, 2.0),
-                  blurRadius: 5.0,
-                ),
-              ],
-              borderRadius: BorderRadius.circular(10.0),
-            ),
+          Text(
+            "Fernando",
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
           ),
         ],
       ),
