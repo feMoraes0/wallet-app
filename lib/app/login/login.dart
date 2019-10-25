6a51421c922c48e087f46594074f916d42845818
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-10-25 02:21:37 +0200

    Updating login screen with new style

--- lib/app/login/login.dart
+++ lib/app/login/login.dart
@@ -1,22 +1,74 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Container(
-        color: Colors.orangeAccent,
+        color: Colors.deepPurpleAccent,
         padding: EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              width: size.width,
-              height: 100,
-              color: Colors.blue,
-            ),
+                width: size.width,
+                padding: const EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.person_pin, color: Colors.deepPurpleAccent),
+                          hintText: "Username",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0)
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0)
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.vpn_key, color: Colors.deepPurpleAccent),
+                          hintText: "Password",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0)
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepPurpleAccent, width: 2.0)
+                          ),
+                        ),
+                      ),
+                    ),
+                    Material(
+                      color: Colors.deepPurpleAccent,
+                      borderRadius: BorderRadius.circular(5.0),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+                          child: Text("Login", style: TextStyle(color: Colors.white, fontSize: 16.0),),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
           ],
         ),
       ),
