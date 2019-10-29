a7a8e7f2cf0ab7bc2362693ec10b8dee843d430e
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-10-29 18:09:09 +0100

    Adding form to wallet screen

--- lib/app/wallet/wallet.dart
+++ lib/app/wallet/wallet.dart
@@ -2,7 +2,6 @@ import "package:flutter/material.dart";
 import 'package:my_wallet/app/components/custom_header.dart';
 import 'package:my_wallet/app/wallet/custom_card.dart';
 
-
 class Wallet extends StatefulWidget {
   @override
   _WalletState createState() => _WalletState();
@@ -15,6 +14,105 @@ class _WalletState extends State<Wallet> {
     this.setState(() {
       this.page = newPage;
     });
+  }
+
+  Future<void> _neverSatisfied() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Title",
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 15.0),
+                            child: TextField(
+                              keyboardType: TextInputType.text,
+                              decoration: InputDecoration(
+                                hoverColor: Theme.of(context).primaryColor,
+                                contentPadding: const EdgeInsets.all(15.0),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.grey[300],
+                                    width: 2.0,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                    width: 2.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            "Value",
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 15.0),
+                            child: TextField(
+                              keyboardType:
+                                  TextInputType.numberWithOptions(decimal: true),
+                              decoration: InputDecoration(
+                                hoverColor: Theme.of(context).primaryColor,
+                                contentPadding: const EdgeInsets.all(15.0),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.grey[300],
+                                    width: 2.0,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Theme.of(context).primaryColor,
+                                    width: 2.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ),
+
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Save',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Theme.of(context).primaryColor,
+                  )),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -46,40 +144,52 @@ class _WalletState extends State<Wallet> {
             Expanded(
               child: Container(
                 width: size.width,
-                padding: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 25.0),
+                padding:
+                    const EdgeInsets.only(top: 10.0, left: 25.0, right: 25.0),
                 child: ListView(
                   children: <Widget>[
                     Container(
                       margin: const EdgeInsets.only(bottom: 10.0),
                       decoration: BoxDecoration(
                         border: Border(
-                          bottom: BorderSide(
-                            color: Theme.of(context).primaryColor,
-                            width: 1.0
-                          ),
+                          bottom: BorderSide(color: Colors.black26, width: 2.0),
                         ),
                       ),
                       child: ListTile(
                         title: Text("Test", style: TextStyle(fontSize: 18.0)),
-                        subtitle: Text("00/00/0000", style: TextStyle(fontSize: 14.0),),
-                        trailing: Text("\$ 100.0", style: TextStyle(fontSize: 18.0, color: Colors.green,),),
+                        subtitle: Text(
+                          "00/00/0000",
+                          style: TextStyle(fontSize: 14.0),
+                        ),
+                        trailing: Text(
+                          "\$ 100.0",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.green,
+                          ),
+                        ),
                       ),
                     ),
                     Container(
                       margin: const EdgeInsets.only(bottom: 10.0),
                       decoration: BoxDecoration(
                         border: Border(
-                          bottom: BorderSide(
-                              color: Theme.of(context).primaryColor,
-                              width: 1.0
-                          ),
+                          bottom: BorderSide(color: Colors.black26, width: 2.0),
                         ),
                       ),
                       child: ListTile(
                         title: Text("Test 2", style: TextStyle(fontSize: 18.0)),
-                        subtitle: Text("00/00/0000", style: TextStyle(fontSize: 14.0),),
-                        trailing: Text("\$ 100.0", style: TextStyle(fontSize: 18.0, color: Theme.of(context).primaryColor,),),
+                        subtitle: Text(
+                          "00/00/0000",
+                          style: TextStyle(fontSize: 14.0),
+                        ),
+                        trailing: Text(
+                          "\$ 100.0",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
                       ),
                     ),
                   ],
@@ -93,6 +203,7 @@ class _WalletState extends State<Wallet> {
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           print("adding new...");
+          this._neverSatisfied();
         },
         icon: Icon(
           Icons.add,
