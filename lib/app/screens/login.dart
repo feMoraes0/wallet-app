ee52247debf7e04d5b77ed1c2465bb1c55d896cc
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-10-30 15:52:41 +0100

    Adding user feedback to login screen

--- lib/app/screens/login.dart
+++ lib/app/screens/login.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
@@ -13,7 +14,8 @@ class _LoginState extends State<Login> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  void getUser(String email, String password) async {
+  Future<void> getUser(
+      String email, String password, BuildContext localContext) async {
     final response = await http.post(
       "https://mywallet-api.herokuapp.com/login",
       headers: {"Content-Type": "application/json"},
@@ -21,15 +23,47 @@ class _LoginState extends State<Login> {
     );
 
     final status = response.statusCode;
-    final body = json.decode(response.body);
-    print(body["user"]["id"]);
 
-    if(status == 200) {
-      Navigator.of(context).pushNamed("home", arguments: {'id': body["user"]["id"]});
-    } else if(status == 400) {
-      // user error
+    if (status == 200) {
+      final body = json.decode(response.body);
+      Navigator.of(context).pushNamed(
+        "home",
+        arguments: {'id': body["user"]["id"]},
+      );
+    } else if (status == 400) {
+      final body = json.decode(response.body);
+      Scaffold.of(localContext).showSnackBar(
+        SnackBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          content: Text(
+            body["msg"],
+            style: TextStyle(color: Colors.white, fontSize: 16.0),
+          ),
+          action: SnackBarAction(
+            label: "Undo",
+            textColor: Colors.white,
+            onPressed: () {
+
+            },
+          ),
+        ),
+      );
     } else {
-      // server error
+      Scaffold.of(localContext).showSnackBar(
+        SnackBar(
+          content: Text(
+            "Server side error, try again later.",
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+          action: SnackBarAction(
+            label: "Undo",
+            textColor: Colors.white,
+            onPressed: () {
+
+            },
+          ),
+        ),
+      );
     }
   }
 
@@ -38,114 +72,114 @@ class _LoginState extends State<Login> {
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: size.height,
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  "Wallet App",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w700,
+      body: Builder(
+        builder: (context) => SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              height: size.height,
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Wallet App",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w700,
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 40.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Email",
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 15.0),
-                        child: TextField(
-                          controller: this._emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(
-                            hoverColor: Theme.of(context).primaryColor,
-                            contentPadding: const EdgeInsets.all(15.0),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.grey[300],
-                                width: 2.0,
+                  Container(
+                    margin: const EdgeInsets.only(top: 40.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Email",
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 15.0),
+                          child: TextField(
+                            controller: this._emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                              hoverColor: Theme.of(context).primaryColor,
+                              contentPadding: const EdgeInsets.all(15.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey[300],
+                                  width: 2.0,
+                                ),
                               ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Theme.of(context).primaryColor,
-                                width: 2.0,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2.0,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20.0, bottom: 25.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        "Password",
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(top: 10.0),
-                        child: TextField(
-                          controller: this._passwordController,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            contentPadding: const EdgeInsets.all(15.0),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.grey[300],
-                                width: 2.0,
+                  Container(
+                    margin: const EdgeInsets.only(top: 20.0, bottom: 25.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Password",
+                          style: TextStyle(fontSize: 18.0),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 10.0),
+                          child: TextField(
+                            controller: this._passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(15.0),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey[300],
+                                  width: 2.0,
+                                ),
                               ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Theme.of(context).primaryColor,
-                                width: 2.0,
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Theme.of(context).primaryColor,
+                                  width: 2.0,
+                                ),
                               ),
                             ),
                           ),
+                        )
+                      ],
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      FocusScope.of(context).unfocus();
+                      String email = this._emailController.text;
+                      String password = this._passwordController.text;
+                      this.getUser(email, password, context);
+                    },
+                    child: Container(
+                      width: size.width,
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: Text(
+                        "Log in",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    String email = this._emailController.text;
-                    String password = this._passwordController.text;
-                    this.getUser(email, password);
-                  },
-                  child: Container(
-                    width: size.width,
-                    padding: const EdgeInsets.symmetric(vertical: 12.0),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    child: Text(
-                      "Log in",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
